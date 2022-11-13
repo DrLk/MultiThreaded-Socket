58fe6833a318675a0b4161008d173caef5028c28
Andrew Ermakovich <ermakovich.andrew@gmail.com>
2022-11-13 15:49:50 +0100

    Update clang-tidy-docker to 16 version

--- clang-tidy-docker/builddocker.sh
+++ clang-tidy-docker/builddocker.sh
@@ -3,5 +3,5 @@
 #export CR_PAT=<TOKEN>
 echo $CR_PAT | docker login ghcr.io -u DrLk --password-stdin
 docker build --no-cache --pull --tag clang-tidy-image .
-docker image tag clang-tidy-image ghcr.io/drlk/clang-tidy-image:v13
-docker image push ghcr.io/drlk/clang-tidy-image:v13
+docker image tag clang-tidy-image ghcr.io/drlk/clang-tidy-image:v16
+docker image push ghcr.io/drlk/clang-tidy-image:v16
