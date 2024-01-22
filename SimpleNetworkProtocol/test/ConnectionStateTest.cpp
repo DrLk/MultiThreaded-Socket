1edd6174f5532c029cd7a052b4338ba17baef946
Ermakovich Andrew <ermakovich.andrew@gmail.com>
2024-01-22 22:37:09 +0100

    Add ConnectionStateTest

--- SimpleNetworkProtocol/test/ConnectionStateTest.cpp
+++ SimpleNetworkProtocol/test/ConnectionStateTest.cpp
@@ -9,6 +9,8 @@
 namespace FastTransport::Protocol {
 
 namespace {
+    constexpr auto TestTimeout = 10s;
+
     std::unordered_map<ConnectionState, std::unique_ptr<IConnectionState>> GetStates()
     {
         std::unordered_map<ConnectionState, std::unique_ptr<IConnectionState>> states;
@@ -20,6 +22,24 @@ namespace {
         states.emplace(ConnectionState::WaitingSynAckState, std::make_unique<WaitingSynAckState>());
         states.emplace(ConnectionState::WaitingSynState, std::make_unique<WaitingSynState>());
         return states;
+    }
+
+    OutgoingPacket::List GetSendPackets(Connection& connection)
+    {
+        auto timeout = std::chrono::steady_clock::now() + TestTimeout;
+        OutgoingPacket::List packets;
+        while (timeout > std::chrono::steady_clock::now()) {
+            packets = connection.GetPacketsToSend();
+            if (!packets.empty()) {
+                break;
+            }
+
+            std::this_thread::sleep_for(100ms);
+        }
+
+        EXPECT_TRUE(timeout > std::chrono::steady_clock::now()) << "Timeout waiting for send packets";
+
+        return packets;
     }
 } // namespace
 
@@ -34,10 +54,10 @@ TEST(ConnectionStateTest, ConnectionState)
     connection.SetInternalFreePackets(UDPQueue::CreateBuffers(10000), UDPQueue::CreateBuffers(10000));
     sourceState = states[sourceState]->SendPackets(connection);
     EXPECT_EQ(sourceState, ConnectionState::WaitingSynAckState);
-    auto packets = connection.GetPacketsToSend();
+    auto packets = GetSendPackets(connection);
 
-    EXPECT_EQ(packets.front().GetPacket()->GetPacketType(), PacketType::Syn);
     EXPECT_EQ(packets.size(), 1);
+    EXPECT_EQ(packets.front().GetPacket()->GetPacketType(), PacketType::Syn);
 
     const ConnectionID destinationID = 1;
     auto [destinationConnection, freePackets] = ListenState::Listen(std::move(packets.front().GetPacket()), destinationID);
@@ -49,7 +69,62 @@ TEST(ConnectionStateTest, ConnectionState)
     EXPECT_EQ(destinationState, ConnectionState::DataState);
 
     {
-        auto packets = destinationConnection->GetPacketsToSend();
+        auto packets = GetSendPackets(*destinationConnection);
+        auto [nextState, freePackets] = states[sourceState]->OnRecvPackets(std::move(packets.front().GetPacket()), connection);
+        sourceState = nextState;
+        EXPECT_EQ(sourceState, ConnectionState::DataState);
+    }
+}
+
+TEST(ConnectionStateTest, ConnectionStateDropSynPacket)
+{
+    auto states = GetStates();
+
+    const ConnectionAddr destinationAddress;
+    const ConnectionID sourceID = 1;
+    auto sourceState = ConnectionState::SendingSynState;
+    Connection connection(sourceState, destinationAddress, sourceID);
+    connection.SetInternalFreePackets(UDPQueue::CreateBuffers(10000), UDPQueue::CreateBuffers(10000));
+    sourceState = states[sourceState]->SendPackets(connection);
+    EXPECT_EQ(sourceState, ConnectionState::WaitingSynAckState);
+
+    auto packets = GetSendPackets(connection);
+
+    EXPECT_EQ(packets.size(), 1);
+    EXPECT_EQ(packets.front().GetPacket()->GetPacketType(), PacketType::Syn);
+
+    {
+        sourceState = states[sourceState]->OnTimeOut(connection);
+        EXPECT_EQ(sourceState, ConnectionState::SendingSynState);
+    }
+
+    {
+        sourceState = states[sourceState]->SendPackets(connection);
+        EXPECT_EQ(sourceState, ConnectionState::WaitingSynAckState);
+
+        packets = GetSendPackets(connection);
+
+        EXPECT_EQ(packets.size(), 1);
+        EXPECT_EQ(packets.front().GetPacket()->GetPacketType(), PacketType::Syn);
+    }
+
+    const ConnectionID destinationID = 1;
+    auto [destinationConnection, freePackets] = ListenState::Listen(std::move(packets.front().GetPacket()), destinationID);
+    EXPECT_EQ(destinationConnection != nullptr, true);
+    destinationConnection->SetInternalFreePackets(UDPQueue::CreateBuffers(10000), UDPQueue::CreateBuffers(10000));
+    auto destinationState = ConnectionState::SendingSynAckState;
+
+    {
+        destinationState = states[destinationState]->SendPackets(*destinationConnection);
+        EXPECT_EQ(destinationState, ConnectionState::DataState);
+
+        packets = GetSendPackets(*destinationConnection);
+
+        EXPECT_EQ(packets.size(), 1);
+        EXPECT_EQ(packets.front().GetPacket()->GetPacketType(), PacketType::SynAck);
+    }
+
+    {
         auto [nextState, freePackets] = states[sourceState]->OnRecvPackets(std::move(packets.front().GetPacket()), connection);
         sourceState = nextState;
         EXPECT_EQ(sourceState, ConnectionState::DataState);
