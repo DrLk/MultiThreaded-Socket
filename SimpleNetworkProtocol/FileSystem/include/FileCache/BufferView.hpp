318fa713e85cb523da35819ea78a6d9947df0128
Ermakovich Andrew <ermakovich.andrew@gmail.com>
2026-03-23 22:16:31 +0100

    Fix clang-tidy

--- SimpleNetworkProtocol/FileSystem/include/FileCache/BufferView.hpp
+++ SimpleNetworkProtocol/FileSystem/include/FileCache/BufferView.hpp
@@ -10,11 +10,11 @@ namespace FastTransport::FileSystem::FileCache {
 struct Span {
     const std::byte* data;
     size_t size;
-};
+} __attribute__((aligned(16)));
 
 struct BufferView {
-    std::vector<Span> spans;
-    RangePin pin;
+    std::vector<Span> spans; // NOLINT(misc-non-private-member-variables-in-classes)
+    RangePin pin; // NOLINT(misc-non-private-member-variables-in-classes)
 
     [[nodiscard]] bool HasData() const noexcept { return !spans.empty(); }
     [[nodiscard]] size_t TotalSize() const noexcept
@@ -25,6 +25,6 @@ struct BufferView {
         }
         return total;
     }
-};
+} __attribute__((aligned(64)));
 
 } // namespace FastTransport::FileSystem::FileCache
--- SimpleNetworkProtocol/FileSystem/include/IPendingJob.hpp
+++ SimpleNetworkProtocol/FileSystem/include/IPendingJob.hpp
@@ -4,6 +4,11 @@ namespace FastTransport::FileSystem {
 
 class IPendingJob {
 public:
+    IPendingJob() = default;
+    IPendingJob(const IPendingJob&) = delete;
+    IPendingJob& operator=(const IPendingJob&) = delete;
+    IPendingJob(IPendingJob&&) = delete;
+    IPendingJob& operator=(IPendingJob&&) = delete;
     virtual ~IPendingJob() = default;
     virtual void Execute() = 0;
 };
